72fd085aa5054d38d8d2db5a6d235ff79a739af2
Danny Peck <dannytpeck@gmail.com>
2017-01-27 09:14:26 -0800

    Add apis

---
+++ api/upload/index.php
@@ -0,0 +1,38 @@
+<?php
+
+	$url = 'https://mywellmetrics.com/api/Upload';
+
+	$params = array(
+		'e' => $_POST['e'],
+		'psk' => $_POST['psk'],
+		'data' => $_POST['data'],
+		'format' => 'csv',
+		'type' => 'challenges',
+		'response' => 'json'
+	);
+
+	$params_string = http_build_query($params);
+
+	// Initialize curl
+	$ch = curl_init();
+
+	// Set curl options
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, count($params));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	// Limeade can take awhile, so let's give it an extra minute
+	set_time_limit(60);
+
+	// Execute post request, store response
+	$response = curl_exec($ch);
+
+	// Close curl connection
+	curl_close($ch);
+
+	// Echo response back to requestor
+	echo $response;
+
+?>
